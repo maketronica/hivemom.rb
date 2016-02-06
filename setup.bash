0d9f50637c578ded7ec67286ccb7d1734d386f90
Jonathan Garvin <jon@5valleys.com>
2016-02-05 21:07:48 -0700

    Add bundle install to setup.bash

--- setup.bash
+++ setup.bash
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-sudo apt-get install sqlite3 libsqlite3-dev
+sudo apt-get install sqlite3 libsqlite3-dev ruby-dev
+sudo gem install bundler --no-rdoc --no-ri
+bundle install --without development test
