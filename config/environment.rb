90d3f425f98a65ed494d57e3c288fc5d8e90078c
Jonathan Garvin <jon@5valleys.com>
2016-02-17 19:46:33 -0700

    Fix travisci settings

--- config/environment.rb
+++ config/environment.rb
@@ -5,10 +5,11 @@ require 'yaml'
 require 'logger'
 require 'active_record'
 
+env = ENV['HIVEMOM_ENV'] || 'development'
+
 Dir.glob(File.expand_path('../../lib/**/*.rb', __FILE__))
    .each { |file| require file }
-require_relative '../config/hivemom.rb'
+require_relative '../config/hivemom.rb' unless env == 'test'
 
-env = ENV['HIVEMOM_ENV'] || 'development'
 db_config = YAML.load_file(File.expand_path('../database.yml', __FILE__))[env]
 ActiveRecord::Base.establish_connection(db_config)
