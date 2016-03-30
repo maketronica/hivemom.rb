05cb8a7430dcc34520495d33c5283db6ac677523
Jonathan Garvin <jon@5valleys.com>
2016-03-29 18:19:03 -0600

    improve error logging

--- scripts/daemon.rb
+++ scripts/daemon.rb
@@ -5,8 +5,10 @@ begin
   Dir.chdir HiveMom.root
   Rack::Server.start(app: HiveMom::Server.new, Host: '192.168.2.1')
 rescue Exception => e
-  HiveMom.logger.fatal("Mom died with: #{e.message}")
-  HiveMom.logger.fatal(e.backtrace.inspect)
+  HiveMom.logger.fatal("Mom died with: #{e} : #{e.message}")
+  e.backtrace.each do |line|
+    HiveMom.logger.fatal(line)
+  end
   raise e
 ensure
   HiveMom.logger.info('Shutting Down')
