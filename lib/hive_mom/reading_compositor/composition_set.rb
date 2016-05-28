bafb3f36996e72781da0ea0907048bd637a77038
Jonathan Garvin <jon@5valleys.com>
2016-05-28 16:35:34 -0600

    reset previous attempt count

--- lib/hive_mom/reading_compositor/composition_set.rb
+++ lib/hive_mom/reading_compositor/composition_set.rb
@@ -21,6 +21,7 @@ module HiveMom
         reading_constructor.pluck(:hive_id).uniq.each do |hive_id|
           begin
             composition_constructor.new(hive_id, self).update
+            reset_previous_attempt_count
           rescue ActiveRecord::StatementInvalid => e
             process_rescued_error(e)
           end
@@ -28,6 +29,10 @@ module HiveMom
       end
 
       private
+
+      def reset_previous_attempt_count
+        @previous_attempt_count = 0
+      end
 
       def process_rescued_error(e)
         raise e unless e.message =~ /SQLite3::BusyException/
